c447ac111ce51fff731acfbafc783907a93ce3d8
Aqil <aqil.tijani@gmail.com>
2023-09-18 09:37:08 -0500

    Aqil Tijani is cool

---
+++ src/Input2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Input2
+{
+    public static void main (String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("What's you're first name? ");
+        String str1= sc.nextLine();
+        System.out.print("You're first name is: "+str1);
+
+
+        System.out.print("What's you're last name? ");
+        String str2= sc.nextLine();
+
+        System.out.print("You're last name is: "+str2);
+    }
+}
